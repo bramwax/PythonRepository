3eb585ee1001af6f7090066aefda84959bc38890
bramwax <andy_bramwell@hotmail.com>
2024-01-05 18:02:02 +0100

    more changes

--- fundamentals/arrays/matrix_traversal/solutions/col_wise.py
+++ fundamentals/arrays/matrix_traversal/solutions/col_wise.py
@@ -1,13 +1,7 @@
-# Column-wise Traversal:
-# ------------------------------------------------------------------------------------------
-# Forward: Iterate through each column from top-left to bottom-right.  
-# ['🍌','👺','🚘','🌆','🍎','🍺','🦑','🛹','😃','🍩','🚆','🕺','🐉','🚴','🏝️','🍕']
-
+# Iterates through each column from top-left to bottom-right.  
 def col_wise_fwd(matrix):
     return "col_wise_fwd executed"
 
-# Reverse: Iterate through each column from bottom-left to top-right.  
-# ['🌆','🚘','👺','🍌','🛹','🦑','🍺','🍎','🕺','🚆','🍩','😃','🍕','🏝️','🚴','🐉']
-
+# Iterates through each column from bottom-left to top-right.  
 def col_wise_rev(matrix):
     return "col_wise_rev executed"
