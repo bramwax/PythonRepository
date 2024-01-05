e025d72895a5c81d28c9628e30f66727d0975cba
bramwax <andy_bramwell@hotmail.com>
2024-01-05 10:20:01 +0100

    Completed row_wise solutions

--- fundamentals/arrays/matrix_traversal/solutions/row_wise.py
+++ fundamentals/arrays/matrix_traversal/solutions/row_wise.py
@@ -1,27 +1,24 @@
-# Row-wise Traversal:
-# ------------------------------------------------------------------------------------------
 # Iterates through each row of matrix from left to right.
-# ['🍌','🍎','😃','🐉','👺','🍺','🍩','🚴','🚘','🦑','🚆','🏝','🌆','🛹','🕺','🍕']
-
 def row_wise_fwd(matrix):
     if len(matrix) == 0:
         return None
     else:
         wid, hgt = len(matrix[0]), len(matrix)
         iteration = []
-        for x in range(0, hgt):
-            for y in range(0, wid):
-                iteration.append(matrix[x][y])
+        for row in range(0, hgt):
+            for col in range(0, wid):
+                iteration.append(matrix[row][col])
         return iteration
 
 
-# Reverse: Iterate through each row from right to left.
-# ['🐉','😃','🍎','🍌','🚴','🍩','🍺','👺','🏝','🚆','🦑','🚘','🍕','🕺','🛹','🌆']
-
+# Iterates through each row from right to left.
 def row_wise_rev(matrix):
-    return "row_wise_rev executed"
-
-matrix = [['🍌','🍎','😃','🐉'],
-          ['👺','🍺','🍩','🚴'],
-          ['🚘','🦑','🚆','🏝'],
-          ['🌆','🛹','🕺','🍕']]
+    if len(matrix) == 0:
+        return None
+    else:
+        wid, hgt = len(matrix[0]), len(matrix)
+        iteration = []
+        for row in range(0, hgt):
+            for col in range(wid - 1, -1, -1):
+                iteration.append(matrix[row][col])
+        return iteration
